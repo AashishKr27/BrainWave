eb40f98f156899cbc9dab02eefd87be1ae51177f
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2024-08-06 23:09:48 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 // import React from 'react'
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 // import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -12,6 +17,11 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
